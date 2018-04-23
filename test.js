90b4c59e29ae0a53c0f331fca2fd1713ffc866ca
durgaprasad_sanagana@yahoo.com <durgaprasad_sanagana@yahoo.com>
2018-04-23 01:44:32 -0500

    adding treant files

---
+++ test.js
@@ -0,0 +1,17 @@
+chart_config = {
+    chart: {
+        container: "#tree-simple"
+    },
+    
+    nodeStructure: {
+        text: { name: "Parent node" },
+        children: [
+            {
+                text: { name: "First child" }
+            },
+            {
+                text: { name: "Second child" }
+            }
+        ]
+    }
+};
